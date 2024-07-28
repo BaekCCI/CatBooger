9c8ab7fe20e39859c66fcd41fc9baa801a5a5c80
ImNotYejun <yejun6210@gmail.com>
2024-07-28 17:26:17 +0900

    태그 선택 기능 추가

--- screen/Community/Comunity.jsx
+++ screen/Community/Comunity.jsx
@@ -30,39 +30,174 @@ const Posts = [
 /** 태그 데이터 */
 const initialAnimalTags = [
   { name: "강아지", isSelected: true },
-  { name: "고양이", isSelected: false },
-  { name: "기타", isSelected: true },
+  { name: "고양이", isSelected: true },
 ];
 const initialCategoryTags = [
   { name: "QnA", isSelected: true },
-  { name: "건강", isSelected: false },
-  { name: "간식", isSelected: false },
+  { name: "건강", isSelected: true },
+  { name: "간식", isSelected: true },
   { name: "일상", isSelected: true },
-  { name: "기타", isSelected: false },
 ];
 
 const Community = ({ navigation }) => {
   const [animalTags, setAnimalTags] = useState(initialAnimalTags);
   const [categoryTags, setCategoryTags] = useState(initialCategoryTags);
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const handleTagPress = (tags, setTags, index) => {
+  const [isFilterOpened, setisFilterOpened] = useState(false);
+  const openFilter = () => {
+    setisFilterOpened(true);
+  };
+  const closeFilter = () => {
+    setisFilterOpened(false);
+  };
+
+  /**커뮤니티 창에서 태그를 누르면 태그를 비활성화 상태로 바꾸는 함수 */
+  const SelectTag = (tags, setTags, index) => {
     const updatedTags = tags.map((tag, i) =>
       i === index ? { ...tag, isSelected: !tag.isSelected } : tag
     );
     setTags(updatedTags);
   };
+  
+  const CommunityTopContainer = ({ openFilter }) => (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+      }}
+    >
+      <Text style={{ fontSize: 30 }}>커뮤니티</Text>
+      <View style={{ flexDirection: 'row', gap: 10 }}>
+        <TouchableOpacity onPress={() => alert("검색창 실행")}>
+          <Image source={require('../../assets/community/search_logo.png')} style={{ width: 25, height: 25 }} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openFilter}>
+          <Image source={require('../../assets/community/filter_logo.png')} style={{ width: 25, height: 25 }} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+  
+  const CommunityTagsContainer = ({ animalTags, setAnimalTags, categoryTags, setCategoryTags, onTagPress }) => (
+    <View style={{ gap: 3, flexDirection : 'row', flexWrap : 'wrap' }}>
+      <View style={{ flexDirection: "row", gap: 5 }}>
+        {animalTags.map((tag, index) => (
+          tag.isSelected ? (
+            <TouchableOpacity key={index} onPress={() => onTagPress(animalTags, setAnimalTags, index)}>
+              <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
+                <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
+              </View>
+            </TouchableOpacity>
+          ) : null
+        ))}
+      </View>
+      <View style={{ flexDirection: "row", gap: 5 }}>
+        {categoryTags.map((tag, index) => (
+          tag.isSelected ? (
+            <TouchableOpacity key={index} onPress={() => onTagPress(categoryTags, setCategoryTags, index)}>
+              <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
+                <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
+              </View>
+            </TouchableOpacity>
+          ) : null
+        ))}
+      </View>
+    </View>
+  );
+  
+  /**필터 모달창에 해당하는 태그 */
+  const FilterModalTag = () => {
+    return(
+      <Modal 
+            visible={isFilterOpened} 
+            onRequestClose={closeFilter}
+            transparent={true}
+            animationType="fade">
+            <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+              <FilterContainer>
+                <FilterContent>
+                  {/**모달창 윗 부분 */}
+                  <View style={{flexDirection : 'row', alignItems : 'baseline'}}>
+                    <TouchableOpacity onPress={closeFilter}> 
+                      <Image source={require("../../assets/community/x_icon.png")} style={{width:20, height : 20}}/>
+                    </TouchableOpacity>
+                    <Text style={{fontSize:25, textAlign : 'center'}}>
+                      필터
+                    </Text>
+                  </View> 
 
-  const openFilter = () => {
-    setIsModalVisible(true);
-  };
+                  <HorizontalLine/>
 
-  const closeFilter = () => {
-    setIsModalVisible(false);
-  };
+                  {/**동물 태그 부분 */}
+                  <Text style={{fontSize : 18}}>동물</Text>
+                  
+                  <HorizontalLine/>
+                  <View style={{ flexDirection: "row", gap: 5 , flexWrap : "wrap"}}>
+                    {animalTags.map((tag, index) => (
+                        <TouchableOpacity key={index} onPress={() => {SelectTag(animalTags, setAnimalTags, index)}}>
+                          <View style={{ borderWidth: 2,borderColor: tag.isSelected ? '#139989' : '#D9D9D9', borderRadius: 5, width: 50, height: 25 }}>
+                            <Text style={{ textAlign: 'center', color : tag.isSelected ? '#139989' : 'black'}}>{tag.name}</Text>
+                          </View>
+                        </TouchableOpacity>
+                    ))}
+                  </View>
+                  
+                  {/**카테고리 태그 부분 */}
+                  <Text style={{fontSize : 18, marginTop : 10}}>카테고리</Text>
+
+                  <HorizontalLine/>
+                  
+                  <View style={{ flexDirection: "row", gap: 5, flexWrap : "wrap"}}>
+                    {categoryTags.map((tag, index) => (
+                        <TouchableOpacity key={index} onPress={() => SelectTag(categoryTags, setCategoryTags, index)}>
+                          <View style={{ borderWidth: 2, borderColor: tag.isSelected ? '#139989' : '#D9D9D9', borderRadius: 5, width: 40, height: 25 }}>
+                            <Text style={{ textAlign: 'center', color : tag.isSelected ? '#139989' : 'black'}}>{tag.name}</Text>
+                          </View>
+                        </TouchableOpacity>
+                    ))}
+                  </View>
+                  
+                  {/**선택 태그 확인 부분 */}
+                  <HorizontalLine/>
+                  <View style={{ gap: 3, flexDirection : 'row', flexWrap : 'wrap'}}>
+                    {/**동물 태그 */}
+                    <View style={{ flexDirection: "row", gap: 5 }}>
+                      {animalTags.map((tag, index) => (
+                        tag.isSelected ? (
+                          <TouchableOpacity key={index} onPress={() => SelectTag(animalTags, setAnimalTags, index)}>
+                            <View style={{ borderWidth: 1, borderColor: '#139989', borderRadius: 5, backgroundColor : '#139989',width: 50, height: 25 }}>
+                              <Text style={{ textAlign: 'center', color : 'white' }}>{tag.name}</Text>
+                            </View>
+                          </TouchableOpacity>
+                        ) : null
+                      ))}
+                    </View>
+                    
+                    {/**카테고리 태그*/}
+                    <View style={{ flexDirection: "row", gap: 5 }}>
+                      {categoryTags.map((tag, index) => (
+                        tag.isSelected ? (
+                          <TouchableOpacity key={index} onPress={() => SelectTag(categoryTags, setCategoryTags, index)}>
+                            <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, backgroundColor : '#139989',width: 50, height: 25 }}>
+                              <Text style={{ textAlign: 'center', color : 'white'}}>{tag.name}</Text>
+                    </View>
+                        </TouchableOpacity>
+                        ) : null
+                    ))}
+                    </View>
+                  </View>
+                </FilterContent>
+              </FilterContainer>
+            </View>
+          </Modal>
+
+    )
+    
+  }
 
   const MoveToPost = () => navigation.navigate('CommunityContentWindow');
-
+  
   return (
     <SafeAreaView>
       
@@ -76,7 +211,7 @@ const Community = ({ navigation }) => {
             setAnimalTags={setAnimalTags}
             categoryTags={categoryTags}
             setCategoryTags={setCategoryTags}
-            onTagPress={handleTagPress}
+            onTagPress={SelectTag}
           />
 
           {Posts.map((PostData, index) => (
@@ -94,134 +229,14 @@ const Community = ({ navigation }) => {
             </Post>
           ))}
 
-        {/* 필터 모달 */}
-        <Modal 
-          visible={isModalVisible} 
-          onRequestClose={closeFilter}
-          transparent={true}
-          animationType="fade">
-          <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-            <FilterContainer>
-              <FilterContent>
-                <View style={{flexDirection : 'row', alignItems : 'baseline'}}>
-                  <TouchableOpacity onPress={closeFilter}> 
-                    <Image source={require("../../assets/community/x_icon.png")} style={{width:20, height : 20}}/>
-                  </TouchableOpacity>
-                  <Text style={{fontSize:25, textAlign : 'center'}}>
-                    필터
-                  </Text>
-                </View> 
+        <FilterModalTag/>
 
-                <HorizontalLine/>
-
-                <Text style={{fontSize : 18}}>동물</Text>
-                
-                <HorizontalLine/>
-                <View style={{ flexDirection: "row", gap: 5 , flexWrap : "wrap"}}>
-                  {animalTags.map((tag, index) => (
-                      <TouchableOpacity key={index} onPress={() => alert(tag.name + " 선택!")}>
-                        <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
-                          <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-                        </View>
-                      </TouchableOpacity>
-                  ))}
-                </View>
-                
-                <Text style={{fontSize : 18, marginTop : 10}}>카테고리</Text>
-
-                <HorizontalLine/>
-
-                <View style={{ flexDirection: "row", gap: 5, flexWrap : "wrap"}}>
-                  {categoryTags.map((tag, index) => (
-                      <TouchableOpacity key={index} onPress={() => alert(tag.name + " 선택!")}>
-                        <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 40, height: 25 }}>
-                          <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-                        </View>
-                      </TouchableOpacity>
-                  ))}
-                </View>
-                
-                <HorizontalLine/>
-                <View style={{ gap: 3 }}>
-                  <View style={{ flexDirection: "row", gap: 5 }}>
-                    {animalTags.map((tag, index) => (
-                      tag.isSelected ? (
-                        <TouchableOpacity key={index} onPress={() => handleTagPress(animalTags, setAnimalTags, index)}>
-                          <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
-                            <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-                          </View>
-                        </TouchableOpacity>
-                      ) : null
-                    ))}
-                  </View>
-                  <View style={{ flexDirection: "row", gap: 5 }}>
-                    {categoryTags.map((tag, index) => (
-                      tag.isSelected ? (
-                        <TouchableOpacity key={index} onPress={() => handleTagPress(categoryTags, setCategoryTags, index)}>
-                          <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
-                            <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-                 </View>
-                        </TouchableOpacity>
-                      ) : null
-                    ))}
-                  </View>
-                </View>
-              </FilterContent>
-            </FilterContainer>
-          </View>
-        </Modal>
         </CommunityContainer>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const CommunityTopContainer = ({ openFilter }) => (
-  <View
-    style={{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'baseline',
-    }}
-  >
-    <Text style={{ fontSize: 30 }}>커뮤니티</Text>
-    <View style={{ flexDirection: 'row', gap: 10 }}>
-      <TouchableOpacity onPress={() => alert("검색창 실행")}>
-        <Image source={require('../../assets/community/search_logo.png')} style={{ width: 25, height: 25 }} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={openFilter}>
-        <Image source={require('../../assets/community/filter_logo.png')} style={{ width: 25, height: 25 }} />
-      </TouchableOpacity>
-    </View>
-  </View>
-);
-
-const CommunityTagsContainer = ({ animalTags, setAnimalTags, categoryTags, setCategoryTags, onTagPress }) => (
-  <View style={{ gap: 3 }}>
-    <View style={{ flexDirection: "row", gap: 5 }}>
-      {animalTags.map((tag, index) => (
-        tag.isSelected ? (
-          <TouchableOpacity key={index} onPress={() => onTagPress(animalTags, setAnimalTags, index)}>
-            <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
-              <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-            </View>
-          </TouchableOpacity>
-        ) : null
-      ))}
-    </View>
-    <View style={{ flexDirection: "row", gap: 5 }}>
-      {categoryTags.map((tag, index) => (
-        tag.isSelected ? (
-          <TouchableOpacity key={index} onPress={() => onTagPress(categoryTags, setCategoryTags, index)}>
-            <View style={{ borderWidth: 1, borderColor: 'green', borderRadius: 5, width: 50, height: 25 }}>
-              <Text style={{ textAlign: 'center' }}>{tag.name}</Text>
-            </View>
-          </TouchableOpacity>
-        ) : null
-      ))}
-    </View>
-  </View>
-);
 
 const CommunityContainer = styled.View`
   border-radius: 5px;
