a23dab963d72cbcd0c05a4dd6f7f9964de837b04
dyfzl <103684409+dyfzl@users.noreply.github.com>
2024-07-31 23:45:40 +0900

    home db

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,6 @@ import { getDatabase } from "firebase/database";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
-
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
